e6dad1506fc659349f07d2fe8dcd02b52eb8bbaa
root <menheimgeorges83@gmail.com>
2024-11-26 16:55:46 +0100

    Begin to program _printf function in a only computer

--- printf.c
+++ printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
  *
@@ -12,4 +12,31 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list box;
+	int i, j;
+	char *car;
+
+	va_start(box, format);
+	j = 0;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		j += va_arg(box, int);
+		if (format[i] != '%')
+		{
+			putchar(format[i]);
+			i++;
+		}
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			putchar(format[i]);
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			car = va_arg(box, char);
+			putchar(car[i]);
+		}
+	}
+	va_end(box);
+	return(j);
+}
 
