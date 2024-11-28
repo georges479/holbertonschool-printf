014eaf9f3be60d57228ec0da81c5eb2352b0e3c2
root <menheimgeorges83@gmail.com>
2024-11-28 16:19:31 +0100

    d_printf to debug

--- d_printf.c
+++ d_printf.c
@@ -3,45 +3,48 @@
 #include <stdio.h>
 
 /**
- * _printf - A simplified version of printf that handles %c, %s, and %%
+* _printf - A simplified version of printf that handles %c, %s, and %%
  * @format: The format string
  * Return: The number of characters printed (excluding the null byte)
  */
 int d_printf(const char *format, ...)
 {
-	va_list box;
-	int i, j, c;
-	char num[20];
+        va_list box;
+        int i, j, c, count, afficheur;
+        char num[10];
 
-	va_start(box, format);
-	j = 0;
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
-		{
-			j += (va_arg(box, int));
-			if (i < 0)
-			{
-				_putchar('-');
-				i = -i;
-			}
-			c = i;
-			while (c % 10)
-			{
-				if (c != 0)
-				{
-					_putchar((c + 1) /  10);
-					c++;
-				}
-				_putchar('\0');
-			}
-			for (c = 0; num[c] < '\0'; c++)
-			{
-				_putchar(num[c++]);
-				j++;
-			}
-		}
-	}
-	va_end(box);
-	return (j);
+        va_start(box, format);
+        j = 0;
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+                {
+                        c = va_arg(box, int);
+                        if (c < 0)
+                        {
+                                putchar('-');
+                                c = -c;
+                        }
+                        else if (c == 0)
+                        {
+                                putchar('0');
+                        }
+                        else
+                        {
+                                count = 9;
+                                while (c > 0)
+                                {
+                                        num[count--] = (char) ('0' + (c % 10));
+                                        c /= 10;
+                                }
+                                for (afficheur = count + 1; afficheur <= 9; afficheur++)
+                                {
+                                        putchar(num[afficheur]);
+                                        j++;
+                                }
+                        }
+                }
+        }
+        va_end(box);
+        return (j);
 }
--- main.c
+++ main.c
@@ -14,13 +14,13 @@ int main(void)
     unsigned int ui;
     void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
+    len = d_printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
+    d_printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
+    d_printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
@@ -36,7 +36,7 @@ int main(void)
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
+    d_printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
