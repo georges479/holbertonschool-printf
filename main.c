8956a978a97b54176364ec549b1f8db884017ad6
root <menheimgeorges83@gmail.com>
2024-11-29 13:37:39 +0100

    Interger part for printf

--- main.c
+++ main.c
@@ -14,13 +14,13 @@ int main(void)
     unsigned int ui;
     void *addr;
 
-    len = d_printf("Let's try to printf a simple sentence.\n");
+    len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
-    d_printf("Length:[%d, %i]\n", len, len);
+    _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
-    d_printf("Negative:[%d]\n", -762534);
+    _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
@@ -36,7 +36,7 @@ int main(void)
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
-    d_printf("Len:[%d]\n", len);
+    _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
