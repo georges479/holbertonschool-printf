94ac0e9612a1dd96d9ca260645478abcc2c031cc
georges479 <g.ismael071221@gmail.com>
2025-03-27 11:55:05 +0100

    correction of _printf.c

--- _print.c
+++ _print.c
@@ -20,7 +20,9 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+		else if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
 			if (format[i] == 'c')
